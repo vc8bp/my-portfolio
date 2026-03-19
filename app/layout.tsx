af8a1505cccd4499667e3877dc1c21824e3101e1
vivek chaturvedi <vc8bp3@gmail.com>
2026-03-19 16:29:10 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,6 +68,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="3vb7PAIymy2lb7q-rHeIelFrbOTQRl4TX_VvfPAxerc" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-B55519Z9TP"></script>
         <script
           dangerouslySetInnerHTML={{
